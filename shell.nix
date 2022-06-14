3414c04353ef8032a41df2713f7d76b60ddbf91f
chesedo <pieter@chesedo.me>
2022-06-14 21:30:57 +0200

    feat: meth_why

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
 { pkgs ? import <nixpkgs> { } }:
 pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [ tectonic biber ];
+  nativeBuildInputs = with pkgs; [ tectonic biber jdk11];
   buildInputs = with pkgs; [ texlab ];
 
   packages = with pkgs; [ zathura ];
