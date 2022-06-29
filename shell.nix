f58de9957177500449b74fa4b920c655f041a036
chesedo <pieter@chesedo.me>
2022-06-29 08:06:17 +0200

    env: downgrade biber

--- shell.nix
+++ shell.nix
@@ -1,7 +1,16 @@
-{ pkgs ? import <nixpkgs> { } }:
-pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [ tectonic biber jdk11];
-  buildInputs = with pkgs; [ texlab ];
-
-  packages = with pkgs; [ zathura ];
-}
+let
+  nixpkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/9bc0e974545d5bc4c24e1ed047be0dc4e30e494b.tar.gz") {};
+in
+  with nixpkgs;
+  stdenv.mkDerivation {
+    name = "tectonic_shell";
+    nativeBuildInputs = with nixpkgs; [
+      tectonic
+      biber
+      jdk11
+    ];
+    buildInputs = with nixpkgs; [
+      texlab
+      zathura
+    ];
+  }
